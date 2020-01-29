18a94f3adbf1ef8066755af8c48688be8dac3c5f
Dallas Gale <dallasgale.digital@gmail.com>
2020-01-29 17:00:48 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ApolloClient, { gql } from "apollo-boost";
 import { ApolloProvider, Query } from "react-apollo";
+import "./App.scss";
 
 const client = new ApolloClient({
   uri: "/.netlify/functions/graphql"
@@ -14,38 +15,38 @@ const GET_ALBUMS = gql`
     }
   }
 `;
-const LambdaDemo = () => (
-  <ApolloProvider client={client}>
-    <Query query={GET_ALBUMS}>
-      {({ data, loading, error }) => {
-        if (loading) return null;
-        if (error) return `Error! ${error}`;
+// const LambdaDemo = () => (
+//   <ApolloProvider client={client}>
+//     <Query query={GET_ALBUMS}>
+//       {({ data, loading, error }) => {
+//         if (loading) return null;
+//         if (error) return `Error! ${error}`;
 
-        return (
-          <div>
-            {!loading &&
-              data.albums &&
-              data.albums.map(album => (
-                <p key={album.title}>
-                  {album.title} {album.year_released}
-                </p>
-              ))}
-          </div>
-        );
-      }}
-    </Query>
-  </ApolloProvider>
-);
+//         return (
+//           <div>
+//             {!loading &&
+//               data.albums &&
+//               data.albums.map(album => (
+//                 <p key={album.title}>
+//                   {album.title} {album.year_released}
+//                 </p>
+//               ))}
+//           </div>
+//         );
+//       }}
+//     </Query>
+//   </ApolloProvider>
+// );
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
+          <h1 style={{ fontSize: 30 }}>
+            THE <span class="title">ACDC-API</span>.COM
+          </h1>
+          <h2>Comming in 2020!</h2>
         </header>
       </div>
     );
